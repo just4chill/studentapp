5038d84f6c6f0897bb8bdc384bf483dbff68db4d
stonedmind <stonrmind@gmail.com>
2015-04-15 15:25:51 +0530

    added id in response

--- app/controllers/weight_controller.rb
+++ app/controllers/weight_controller.rb
@@ -6,34 +6,34 @@ class WeightController < ApplicationController
   	@weights.each do |weight|
   		node = weight.info.split(",")[0]
   		val = weight.info.split(",")[1]
-  		@resp.push(node,val)
+  		@resp.push(weight.id.to_s,node,val)
   	end
   	render :json => @resp
   end
 
  def create
   	@weight = Weight.create!(weight_params)
-  	@resp = [@weight.info.split(",")[0],@weight.info.split(",")[1]]
+  	@resp = [@weight.id.to_s, @weight.info.split(",")[0],@weight.info.split(",")[1]]
   	render :json => @resp
   end
 
    def update
   	@weight = Weight.find_by!(id: params[:id])
   	@weight.update!(weight_params)
-  	@resp = [@weight.info.split(",")[0],@weight.info.split(",")[1]]
+  	@resp = [@weight.id.to_s, @weight.info.split(",")[0],@weight.info.split(",")[1]]
   	render :json => @resp
   end
 
   def destroy
   	@weight = Weight.find_by!(id: params[:id])
   	@weight.destroy!()
-  	@resp = [@weight.info.split(",")[0],@weight.info.split(",")[1]]
+  	@resp = [@weight.id.to_s, @weight.info.split(",")[0],@weight.info.split(",")[1]]
   	render :json => @resp
   end
 
    def show
   	@weight = Weight.find_by!(id: params[:id])
-  	@resp = [@weight.info.split(",")[0],@weight.info.split(",")[1]]
+  	@resp = [@weight.id.to_s, @weight.info.split(",")[0],@weight.info.split(",")[1]]
   	render :json => @resp
   end
 
