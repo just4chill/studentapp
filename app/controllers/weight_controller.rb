859f3cf31b7dcc81996077ae31c526b322bb54ba
stonedmind <stonrmind@gmail.com>
2015-04-21 22:12:07 +0530

    changed

--- app/controllers/weight_controller.rb
+++ app/controllers/weight_controller.rb
@@ -1,67 +1,36 @@
 class WeightController < ApplicationController
   
-  def index
-<<<<<<< HEAD
+ def index
     render :json => Weight.all()
-=======
-  	@resp = Array.new
-  	@weights = Weight.all()
-  	@weights.each do |weight|
-  		node = weight.info.split(",")[0]
-  		val = weight.info.split(",")[1]
-  		@resp.push(weight.id.to_s,node,val)
-  	end
-  	render :json => @resp
->>>>>>> 5038d84f6c6f0897bb8bdc384bf483dbff68db4d
   end
 
  def create
-  	@weight = Weight.create!(weight_params)
-<<<<<<< HEAD
-  	render :json => @weight
-=======
-  	@resp = [@weight.id.to_s, @weight.info.split(",")[0],@weight.info.split(",")[1]]
-  	render :json => @resp
->>>>>>> 5038d84f6c6f0897bb8bdc384bf483dbff68db4d
+    @weight = Weight.create!(weight_params)
+    render :json => @weight
   end
 
    def update
-  	@weight = Weight.find_by!(id: params[:id])
-  	@weight.update!(weight_params)
-<<<<<<< HEAD
-  	render :json => @weight
-=======
-  	@resp = [@weight.id.to_s, @weight.info.split(",")[0],@weight.info.split(",")[1]]
-  	render :json => @resp
->>>>>>> 5038d84f6c6f0897bb8bdc384bf483dbff68db4d
+    @weight = Weight.find_by!(id: params[:id])
+    @weight.update!(weight_params)
+    render :json => @weight
   end
 
   def destroy
-  	@weight = Weight.find_by!(id: params[:id])
-  	@weight.destroy!()
-<<<<<<< HEAD
-  	render :json => @weight
-=======
-  	@resp = [@weight.id.to_s, @weight.info.split(",")[0],@weight.info.split(",")[1]]
-  	render :json => @resp
->>>>>>> 5038d84f6c6f0897bb8bdc384bf483dbff68db4d
+    @weight = Weight.find_by!(id: params[:id])
+    @weight.destroy!()
+    render :json => @weight
   end
 
    def show
-  	@weight = Weight.find_by!(id: params[:id])
-<<<<<<< HEAD
-  	render :json => @weight
-=======
-  	@resp = [@weight.id.to_s, @weight.info.split(",")[0],@weight.info.split(",")[1]]
-  	render :json => @resp
->>>>>>> 5038d84f6c6f0897bb8bdc384bf483dbff68db4d
+    @weight = Weight.find_by!(id: params[:id])
+    render :json => @weight
   end
 
 
   private
 
   def weight_params
-  	params.require(:weight).permit(:ref, :w_val)
+    params.require(:weight).permit(:ref, :w_val)
   end
   
 end
